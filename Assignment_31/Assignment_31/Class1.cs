96e824979201ff38db1c8cf696aaf02198b316d7
Mihir patel <71175029+Mihir2603@users.noreply.github.com>
2022-02-01 11:50:25 +0530

    Add files via upload

---
+++ Assignment_31/Assignment_31/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Fraction_operation;
+namespace Program
+{
+    class Main_Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please enter the numerator?");
+            int numerator = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the denominator?");
+            int denominator = Convert.ToInt32(Console.ReadLine());
+            Fraction F = new Fraction(numerator, denominator);
+            Console.WriteLine($"Floating point division result = { F.FloatingDivision() }");
+F    .IntegerDivison();
+            F.ReduceFraction();
+        }
+    }
+}
